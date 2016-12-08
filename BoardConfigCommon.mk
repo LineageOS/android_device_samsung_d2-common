888a41420124865ffb5fe1abd80ef6c6b40adccb
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-12-07 19:56:39 -0600

    d2: compress recovery ramdisk with lzma
    Change-Id: I91d36e11debe460ac6fe31d42a62e49d6d49a7ae

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,6 +32,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Kernel
 TARGET_KERNEL_CONFIG        := lineageos_d2_defconfig
+LZMA_RAMDISK_TARGETS        := recovery
 
 # Audio
 BOARD_HAVE_AUDIENCE_A2220 := true
