0515fca065c8ad0669aa687202d3042b2a7d1a96
Paul Keith <javelinanddart@aidenswann.com>
2017-03-09 08:21:34 -0600

    d2: Update for generic extract files
    Change-Id: I5ec7d945186c715b33cc0b65e85039ee7550ac56
    Signed-off-by: Paul Keith <javelinanddart@aidenswann.com>

--- extract-files.sh
+++ extract-files.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-if [ "$1" != "" ]; then
-    export SOURCEDIR=$1
-fi
-
-export DEVICE=d2-common
+export DEVICE_COMMON=d2-common
 export VENDOR=samsung
 ./../msm8960-common/extract-files.sh $@
